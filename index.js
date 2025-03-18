196e5d0743af54bd7e16557487e9f30894ec79d7
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-18 16:14:06 -0500

    random

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-//Math
+// Random number generator
 
-let x=3.21;
-let y=2;
-let z=1;
-// z=Math.round(x);
-// console.log(z);
-// z=Math.ceil(x);
-// console.log(z);
-// z=Math.floor(x);
-// console.log(z);
-
-// z= Math.trunc(x);
-// console.log(z);
-
-// z=Math.sqrt(x);
-// console.log(z);
-
-
-// z=Math.log(x);
-// console.log(z);
-
-// z=Math.sin(x);
-// z=Math.cos(x);
-
-let max=Math.max(x,y,z);
-let min=Math.min(x,y,z);
-console.log(max+" "+min);
+let randomNum = Math.ceil(Math.random()*100)+20;
+console.log(randomNum);
