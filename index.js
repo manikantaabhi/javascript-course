e4f5f5f53b2ab883c4f7e5055b3414c45e392439
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-18 12:43:59 -0500

    Operators

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
+let students=30;
+students+=1;
+students**=2;
+students%=2;
+console.log(students);
 
-let price=10.99;
-console.log(price);
-console.log(typeof price);
-console.log("hello");
-
-let isIt=true;
-console.log(`Is it? :${isIt?"Yes":"No"}`)
-
-let fullName="Manikanta Neerugatti";
-let age=28;
-let student=true;
-
-
-document.getElementById("p1").textContent=`My name is ${fullName}`;
-document.getElementById("p2").textContent=`My age is ${age}`;
-document.getElementById("p3").textContent=`I am student ${student?"Yes":"No"}`;
+// And many more
