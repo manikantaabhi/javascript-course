94405527e2ca3b18536071cc9fc8537563110207
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-18 12:40:20 -0500

    variables

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+
+let price=10.99;
+console.log(price);
+console.log(typeof price);
 console.log("hello");
+
+let isIt=true;
+console.log(`Is it? :${isIt?"Yes":"No"}`)
+
+let fullName="Manikanta Neerugatti";
+let age=28;
+let student=true;
+
+
+document.getElementById("p1").textContent=`My name is ${fullName}`;
+document.getElementById("p2").textContent=`My age is ${age}`;
+document.getElementById("p3").textContent=`I am student ${student?"Yes":"No"}`;
