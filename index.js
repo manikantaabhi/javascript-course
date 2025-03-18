7a5e37c2f7f56ad5df6a5c0d8d98e4c0a3791402
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-18 13:01:07 -0500

    user inputs

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let students=30;
-students+=1;
-students**=2;
-students%=2;
-console.log(students);
+// Accept user inputs
 
-// And many more
+// let userName;
+
+// userName = window.prompt("What is userName:")
+// console.log(userName);
+
+
+// let userName=window.prompt("wnat is userName");
+// console.log(userName);
+
+let userName;
+document.getElementById("myButton").onclick=function(){
+    userName = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent=userName;
+}
+
