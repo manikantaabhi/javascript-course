e6d317e6df272e2b601167987d3d18c1b12c871f
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-19 08:16:46 -0500

    callbacks

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-// Random password generator
-function generatePassword(passwordLength,includeLowerCase,includeUpperCase,includeNumbers, includeSecialChars)
+// callback
+
+
+Hello(GoodBye);
+function Hello(callback)
 {
-    const upperCaseLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowerCaseLetters="abcdefghijklmnopqrstuvwxyz";
-    const numbers="1234567890";
-    const specialChars="~@#$%^&*()_+";
-
-    let password="";
-    let allowedChars=(includeLowerCase?lowerCaseLetters:"")+
-                    (includeUpperCase?upperCaseLetters:"")+
-                    (includeNumbers?numbers:"")+
-                    (includeSecialChars?specialChars:"");
-    for(let i=0;i<passwordLength;i++){
-        let index=Math.floor(Math.random()*allowedChars.length);
-        password+=allowedChars[index];
-    }
-    return password;
+    console.log("Hello");
+    callback();
 }
-
-
-
-let passwordLength=10;
-let includeUpperCase=true;
-let includeLowerCase=true;
-let includeNumbers=true;
-let includeSecialChars=true;
-console.log(generatePassword(passwordLength,includeLowerCase,includeUpperCase,includeNumbers, includeSecialChars));
+function GoodBye(){
+    console.log("Good Bye!");
+}
+// Callbacks are very much useful in eventListeners.(why?: Understand eventListeners first)
+/* we might get this thought of 
+  what if we add setTimeInterval 
+  to console.log in Hello function? */
