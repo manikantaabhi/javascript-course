52c9ec10ad918996b77207fb335a9accbc5f77ef
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-18 21:03:02 -0500

    arrays intro

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-// temperature converter
+ // array
 
-const inputNum=document.getElementById("inputNum");
+ let fruits=["apple","banana","orange"];
 
-const toF=document.getElementById("toFahrenheit");
-const toC=document.getElementById("toCelsius");
-const para=document.getElementById("convertedPara");
+//  fruits.unshift("berry");
+//  fruits.shift();
+//  fruits.push("berry");
+//  fruits.pop();
+let noOfFruits=fruits.length;
+let index=fruits.indexOf("banana");
 
-let temp;
-
-
-function convert(){
-    if(toF.checked){
-        temp=Number(inputNum.value);
-        temp=temp*9/5+32;
-        para.textContent=temp.toFixed(1)+"°F";
-    }
-    else if(toC.checked){
-        temp=Number(inputNum.value);
-        temp=(temp-32)*(5/9);
-        para.textContent=temp.toFixed(1)+"°C";
-    }
-    else
-        para.textContent="Please select conversion type";
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
 }
+
+fruits.forEach(element => {
+    console.log(element);
+});
+
+for(let fruit of fruits)
+    console.log(fruit);
+
+fruits.sort();
+console.log(fruits);
+fruits.sort().reverse();
+ console.log(fruits);
