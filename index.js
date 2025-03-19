696a86560241ca1bd5839d4c6b2e55432600d145
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-19 08:33:59 -0500

    forEach including callbacks

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// callback
+// forEach loop
+
+let numbers=[3,4,5,1,2];
+numbers.forEach((element,index,numbers) => console.log(element+" "+index+" "+numbers));
+
+function double(element, index,numbers){
+    numbers[index]=element*2;
+}
+numbers.forEach(double);
+numbers.forEach(element=>console.log(element));
+
+function cubed(element, index,numbers){
+    numbers[index]=Math.pow(element,3);
+}
+
+numbers.forEach(cubed);
+numbers.forEach(element=> console.log(element));
 
 
-Hello(GoodBye);
-function Hello(callback)
-{
-    console.log("Hello");
-    callback();
-}
-function GoodBye(){
-    console.log("Good Bye!");
-}
-// Callbacks are very much useful in eventListeners.(why?: Understand eventListeners first)
-/* we might get this thought of 
-  what if we add setTimeInterval 
-  to console.log in Hello function? */
+/* forEach method by default provides 3 values on call->
+    element(current element), index(index of the current element)
+    array(entire array) */
+
+/* so if we call any callback emthod inside forEach it internally passes 3 parameters (current element, index of the current element, and the array itself) */
