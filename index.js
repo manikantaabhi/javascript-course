248002c4f5bef84e952b29202eed9e1a08d987a3
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-19 11:38:01 -0500

    arrow functions

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-// reduce method
+// arrow functions
 
-//reduce(accumulator,element) : accumulator is-> initially it is first values and then it is result of the recude method and element is the second value and then subsequent values
+let nums=[1,2,3,4,5,6];
 
-
-let nums=[2,3,4,5,1,3];
-let max=nums.reduce((x,y)=>Math.max(x,y));
-console.log(max);
-
-let min=nums.reduce((x,y)=>Math.min(x,y));
-console.log(min);
-
-let sum=nums.reduce(SumOfArr);
-
-function SumOfArr(acc,ele){
-    return acc+ele;
-}
-
+let sum=nums.reduce((acc,ele)=>acc+ele);
 console.log(sum);
 
-let transactions=[
-    {type:"debit",amount:500},
-    {type:"debit",amount:1000},
-    {type:"credit",amount:500},
-    {type:"credit",amount:500},
-    {type:"debit",amount:1000},
-]
-
-let totalBal=transactions.reduce(calBal,0);
-
-function calBal(acc,transaction){
-    if(transaction.type==="debit")
-    {
-        return acc-transaction.amount;
-    }
-    else if(transaction.type==="credit"){
-        return acc+transaction.amount;
-    }
-    return acc;
+let hello=function(){
+    console.log("Hello");
 }
 
-console.log(totalBal);
+hello();
+
+setTimeout(()=>{
+    console.log("hello timeout");
+},3000);
+
+let doubled = nums.map((ele)=>{
+    return ele*2;
+})
+
+console.log(doubled);
+
+let even = nums.filter((e)=>e%2===0);
+console.log(even);
