844b79fe8445ee51a453bf99edb318c8a176c861
manikantaabhi <manikanta.thayappa@gmail.com>
2025-03-19 09:02:52 -0500

    filter method

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-// map method -- similar to forEach but map returns a new array after processing each element 
-// map helps in preserving the original array
+// filter method
 
-let nums=[4,5,3,8,7];
+let nums=[5,46,3,2,4,5,6];
+let evenNums=nums.filter(e=>e%2===0);
+console.log(evenNums);
 
-let squaredNums=nums.map((e)=>{
-    return e*3;
-});
+let oddNums=nums.filter(e=>e%2!==0);
+console.log(oddNums);
 
-console.log(squaredNums);
+// passing callbacks
+evenNums=nums.filter(evenNum);
 
-let cubedNums=nums.map(cubed);
-console.log(cubedNums);
-
-function cubed(element){
-    return Math.pow(element,3);
+function evenNum(element)
+{
+    return element%2===0;
 }
 
-nums.map(e=>e*2);// it is mapping but we are not doing anything with that.
+console.log(evenNums);
 
-console.log(nums);
+oddNums=nums.filter(oddNum);
 
-nums=nums.map(e=>e*2);// now we can see the difference
-console.log(nums);
-
-
-console.log(nums); // didn't change the original array
-
-nums.forEach(e=>e*2); // why is it not changing the original array? see below
-
-console.log(nums);
-
-//here
-nums.forEach(changeLikeThis);
-
-function changeLikeThis(element, index, array){
-    array[index]=element*2;
+function oddNum(element){
+    return element%2!==0;
 }
+console.log(oddNums);
 
-console.log(nums);// now changed!!!
+const fruits=["banana","kiwi","promogranate"];
+const newFruits=fruits.filter(fruit=>fruit.length<5);
+console.log(newFruits);
